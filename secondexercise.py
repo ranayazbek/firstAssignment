5eb7972cba48cf9699a9a779089e83cb09682ee7
Rana Yazbek <rana.yazbekk@gmail.com>
2025-02-12 22:27:18 +0200

    Create <> thirdexercise.py

--- secondexercise.py
+++ secondexercise.py
@@ -1,6 +1,6 @@
 input= int (input("Enter a number: "))
 
-if input %2 ==0: 
+if input %2 ==0: # % is modulus operator that gives the remainder of the division of the two numbers (input and 2)
     print ("The number is even")
 else: 
     print ("The number is odd")
---
+++ thirdexercise.py
@@ -0,0 +1,8 @@
+username = input(" Enter username:")
+password= input(" Enter password: ")
+
+if username == "admin" and password == "admin":
+    print ("Access granted")
+
+else: 
+    print ("Access denied")
